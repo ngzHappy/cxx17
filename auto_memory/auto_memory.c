eebc5c78f33f5ae30272315c194cf2d344b9e217
ngzHappy <819869472@qq.com>
2017-01-17 14:21:57 +0800

    -build warning

--- auto_memory/auto_memory.c
+++ auto_memory/auto_memory.c
@@ -12,50 +12,50 @@
 
 static struct { int _unamed_[4]; } null_data;
 
-BOOST_SYMBOL_EXPORT void auto_out_of_memory(){
+BOOST_SYMBOL_EXPORT void auto_out_of_memory() {
     std_out_of_memory(-7777);
 }
 
-BOOST_SYMBOL_EXPORT void * auto_malloc(size_t arg){
-    if(arg<1){return &null_data;}
-    void * ans = std_malloc(arg);
-    if(ans==0){
+BOOST_SYMBOL_EXPORT void * auto_malloc(size_t arg) {
+    if (arg<1) { return &null_data; }
+    void * ans=std_malloc(arg);
+    if (ans==0) {
         auto_out_of_memory();
     }
     return ans;
 }
 
-BOOST_SYMBOL_EXPORT void * auto_realloc(void* argO,size_t argOSize,size_t argNSize){
-    if(argO==&null_data){
+BOOST_SYMBOL_EXPORT void * auto_realloc(void* argO,size_t argOSize,size_t argNSize) {
+    if (argO==&null_data) {
         return auto_malloc(argNSize);
     }
-    void * ans = std_realloc(argO,argNSize);
-    if(ans==0){
+    void * ans=std_realloc(argO,argNSize);
+    if (ans==0) {
         auto_out_of_memory();
     }
     return ans;
     (void)argOSize;
 }
 
-BOOST_SYMBOL_EXPORT void * auto_calloc(size_t argN,size_t argS){
-    if((argN<1)||(argS<1)){
+BOOST_SYMBOL_EXPORT void * auto_calloc(size_t argN,size_t argS) {
+    if ((argN<1)||(argS<1)) {
         return &null_data;
     }
-    void * ans = std_calloc(argN,argS);
-    if(ans==0){
+    void * ans=std_calloc(argN,argS);
+    if (ans==0) {
         auto_out_of_memory();
     }
     return ans;
 }
 
-BOOST_SYMBOL_EXPORT void auto_free(void* arg){
-    if(arg==&null_data){
-        return ;
+BOOST_SYMBOL_EXPORT void auto_free(void* arg) {
+    if (arg==&null_data) {
+        return;
     }
-    return std_free(arg);
+    std_free(arg);
 }
 
-BOOST_SYMBOL_EXPORT void auto_memory_gc(){
+BOOST_SYMBOL_EXPORT void auto_memory_gc() {
 
 }
 
